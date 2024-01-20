d125617a7d80b1a322621921c1fa1af5ab298b01
Elisa Calderon <136856759+ElisaCalderon@users.noreply.github.com>
2024-01-20 17:32:45 -0600

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ElisaCalderon/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ElisaCalderon/godesde0/variables"
+)
+
+func main(){
+	variables.MuestroEnteros()
+}
